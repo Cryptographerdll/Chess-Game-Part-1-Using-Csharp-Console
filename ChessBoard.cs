da0928e47ee36ae54138a474dbbc95d9491c2bb5
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-11-15 23:47:11 +0100

    Add files via upload

---
+++ ChessBoard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess_Game
+{
+    public class ChessBoard
+    {
+        private String[,] chessBoard;
+
+        public const int Dimension = 8;
+
+        private PieceMoved pieceMoved;
+        private Move move;
+
+        public ChessBoard()
+        {
+            pieceMoved = new PieceMoved();
+            move = new Move();
+
+            chessBoard = new string[Dimension, Dimension];
+            this.ChessBoardHorizoontalSymbol = "+---";
+            this.ChessBoardVerticalSymbol = "| ";
+        }
+
+        public String ChessBoardHorizoontalSymbol { get; set; }
+        public String ChessBoardVerticalSymbol { get; set; }
+    }
+}
